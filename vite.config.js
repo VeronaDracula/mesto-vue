581493a5d51d1b3cc66e29a917664670ff0f1c10
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2026-09-10 10:34:08 +0300

    deploy code

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/VeronaDracula/mesto-vue.git', 
   plugins: [
     vue(),
     vueDevTools(),
